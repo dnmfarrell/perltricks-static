2deaf55e672182f21c01b9b27acb6189d014b189
David Farrell <davidnmfarrell@gmail.com>
2016-05-23 20:08:13 -0400

    use absolute urls

--- static/widgets/toplinks/toplinks.js
+++ static/widgets/toplinks/toplinks.js
@@ -36,7 +36,7 @@
         css_link = document.createElement('link');
         css_link.setAttribute('rel','stylesheet');
         css_link.setAttribute('type','text/css');
-        css_link.setAttribute('href','/widgets/toplinks/style.css');
+        css_link.setAttribute('href','https://perltricks.com/widgets/toplinks/toplinks.css');
         head.appendChild(css_link);
       }
       else {
